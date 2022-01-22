66b5dc120846de1e3de7958f8684fb63f4e5ae83
Campbell He <campbell.he@icloud.com>
2022-01-22 16:18:29 +0800

    feat: add register description of hstimer

--- src/hstimer.rs
+++ src/hstimer.rs
@@ -6,27 +6,27 @@ pub struct RegisterBlock {
     #[doc = "0x04 - HS Timer Status Register"]
     pub hs_tmr_irq_stas: crate::Reg<hs_tmr_irq_stas::HS_TMR_IRQ_STAS_SPEC>,
     _reserved2: [u8; 0x18],
-    #[doc = "0x20 - HS Timer0 Control Register"]
-    pub hs_tmr0_ctrl: crate::Reg<hs_tmr0_ctrl::HS_TMR0_CTRL_SPEC>,
-    #[doc = "0x24 - HS Timer0 Interval Value Low Register"]
-    pub hs_tmr0_intv_lo: crate::Reg<hs_tmr0_intv_lo::HS_TMR0_INTV_LO_SPEC>,
-    #[doc = "0x28 - HS Timer0 Interval Value High Register"]
-    pub hs_tmr0_intv_hi: crate::Reg<hs_tmr0_intv_hi::HS_TMR0_INTV_HI_SPEC>,
-    #[doc = "0x2c - HS Timer0 Current Value Low Register"]
-    pub hs_tmr0_curnt_lo: crate::Reg<hs_tmr0_curnt_lo::HS_TMR0_CURNT_LO_SPEC>,
-    #[doc = "0x30 - HS Timer0 Current Value High Register"]
-    pub hs_tmr0_curnt_hi: crate::Reg<hs_tmr0_curnt_hi::HS_TMR0_CURNT_HI_SPEC>,
+    #[doc = "0x20 - HS Timer Control Register"]
+    pub hs_tmr0_ctrl: crate::Reg<hs_tmr_ctrl::HS_TMR_CTRL_SPEC>,
+    #[doc = "0x24 - HS Timer Interval Value Low Register"]
+    pub hs_tmr0_intv_lo: crate::Reg<hs_tmr_intv_lo::HS_TMR_INTV_LO_SPEC>,
+    #[doc = "0x28 - HS Timer Interval Value High Register"]
+    pub hs_tmr0_intv_hi: crate::Reg<hs_tmr_intv_hi::HS_TMR_INTV_HI_SPEC>,
+    #[doc = "0x2c - HS Timer Current Value Low Register"]
+    pub hs_tmr0_curnt_lo: crate::Reg<hs_tmr_curnt_lo::HS_TMR_CURNT_LO_SPEC>,
+    #[doc = "0x30 - HS Timer Current Value High Register"]
+    pub hs_tmr0_curnt_hi: crate::Reg<hs_tmr_curnt_hi::HS_TMR_CURNT_HI_SPEC>,
     _reserved7: [u8; 0x0c],
-    #[doc = "0x40 - HS Timer1 Control Register"]
-    pub hs_tmr1_ctrl: crate::Reg<hs_tmr1_ctrl::HS_TMR1_CTRL_SPEC>,
-    #[doc = "0x44 - HS Timer1 Interval Value Low Register"]
-    pub hs_tmr1_intv_lo: crate::Reg<hs_tmr1_intv_lo::HS_TMR1_INTV_LO_SPEC>,
-    #[doc = "0x48 - HS Timer1 Interval Value High Register"]
-    pub hs_tmr1_intv_hi: crate::Reg<hs_tmr1_intv_hi::HS_TMR1_INTV_HI_SPEC>,
-    #[doc = "0x4c - HS Timer1 Current Value Low Register"]
-    pub hs_tmr1_curnt_lo: crate::Reg<hs_tmr1_curnt_lo::HS_TMR1_CURNT_LO_SPEC>,
-    #[doc = "0x50 - HS Timer1 Current Value High Register"]
-    pub hs_tmr1_curnt_hi: crate::Reg<hs_tmr1_curnt_hi::HS_TMR1_CURNT_HI_SPEC>,
+    #[doc = "0x40 - HS Timer Control Register"]
+    pub hs_tmr1_ctrl: crate::Reg<hs_tmr_ctrl::HS_TMR_CTRL_SPEC>,
+    #[doc = "0x44 - HS Timer Interval Value Low Register"]
+    pub hs_tmr1_intv_lo: crate::Reg<hs_tmr_intv_lo::HS_TMR_INTV_LO_SPEC>,
+    #[doc = "0x48 - HS Timer Interval Value High Register"]
+    pub hs_tmr1_intv_hi: crate::Reg<hs_tmr_intv_hi::HS_TMR_INTV_HI_SPEC>,
+    #[doc = "0x4c - HS Timer Current Value Low Register"]
+    pub hs_tmr1_curnt_lo: crate::Reg<hs_tmr_curnt_lo::HS_TMR_CURNT_LO_SPEC>,
+    #[doc = "0x50 - HS Timer Current Value High Register"]
+    pub hs_tmr1_curnt_hi: crate::Reg<hs_tmr_curnt_hi::HS_TMR_CURNT_HI_SPEC>,
 }
 #[doc = "HS_TMR_IRQ_EN register accessor: an alias for `Reg<HS_TMR_IRQ_EN_SPEC>`"]
 pub type HS_TMR_IRQ_EN = crate::Reg<hs_tmr_irq_en::HS_TMR_IRQ_EN_SPEC>;
@@ -36,43 +36,23 @@ pub mod hs_tmr_irq_en;
 pub type HS_TMR_IRQ_STAS = crate::Reg<hs_tmr_irq_stas::HS_TMR_IRQ_STAS_SPEC>;
 #[doc = "HS Timer Status Register"]
 pub mod hs_tmr_irq_stas;
-#[doc = "HS_TMR0_CTRL register accessor: an alias for `Reg<HS_TMR0_CTRL_SPEC>`"]
-pub type HS_TMR0_CTRL = crate::Reg<hs_tmr0_ctrl::HS_TMR0_CTRL_SPEC>;
-#[doc = "HS Timer0 Control Register"]
-pub mod hs_tmr0_ctrl;
-#[doc = "HS_TMR0_INTV_LO register accessor: an alias for `Reg<HS_TMR0_INTV_LO_SPEC>`"]
-pub type HS_TMR0_INTV_LO = crate::Reg<hs_tmr0_intv_lo::HS_TMR0_INTV_LO_SPEC>;
-#[doc = "HS Timer0 Interval Value Low Register"]
-pub mod hs_tmr0_intv_lo;
-#[doc = "HS_TMR0_INTV_HI register accessor: an alias for `Reg<HS_TMR0_INTV_HI_SPEC>`"]
-pub type HS_TMR0_INTV_HI = crate::Reg<hs_tmr0_intv_hi::HS_TMR0_INTV_HI_SPEC>;
-#[doc = "HS Timer0 Interval Value High Register"]
-pub mod hs_tmr0_intv_hi;
-#[doc = "HS_TMR0_CURNT_LO register accessor: an alias for `Reg<HS_TMR0_CURNT_LO_SPEC>`"]
-pub type HS_TMR0_CURNT_LO = crate::Reg<hs_tmr0_curnt_lo::HS_TMR0_CURNT_LO_SPEC>;
-#[doc = "HS Timer0 Current Value Low Register"]
-pub mod hs_tmr0_curnt_lo;
-#[doc = "HS_TMR0_CURNT_HI register accessor: an alias for `Reg<HS_TMR0_CURNT_HI_SPEC>`"]
-pub type HS_TMR0_CURNT_HI = crate::Reg<hs_tmr0_curnt_hi::HS_TMR0_CURNT_HI_SPEC>;
-#[doc = "HS Timer0 Current Value High Register"]
-pub mod hs_tmr0_curnt_hi;
-#[doc = "HS_TMR1_CTRL register accessor: an alias for `Reg<HS_TMR1_CTRL_SPEC>`"]
-pub type HS_TMR1_CTRL = crate::Reg<hs_tmr1_ctrl::HS_TMR1_CTRL_SPEC>;
-#[doc = "HS Timer1 Control Register"]
-pub mod hs_tmr1_ctrl;
-#[doc = "HS_TMR1_INTV_LO register accessor: an alias for `Reg<HS_TMR1_INTV_LO_SPEC>`"]
-pub type HS_TMR1_INTV_LO = crate::Reg<hs_tmr1_intv_lo::HS_TMR1_INTV_LO_SPEC>;
-#[doc = "HS Timer1 Interval Value Low Register"]
-pub mod hs_tmr1_intv_lo;
-#[doc = "HS_TMR1_INTV_HI register accessor: an alias for `Reg<HS_TMR1_INTV_HI_SPEC>`"]
-pub type HS_TMR1_INTV_HI = crate::Reg<hs_tmr1_intv_hi::HS_TMR1_INTV_HI_SPEC>;
-#[doc = "HS Timer1 Interval Value High Register"]
-pub mod hs_tmr1_intv_hi;
-#[doc = "HS_TMR1_CURNT_LO register accessor: an alias for `Reg<HS_TMR1_CURNT_LO_SPEC>`"]
-pub type HS_TMR1_CURNT_LO = crate::Reg<hs_tmr1_curnt_lo::HS_TMR1_CURNT_LO_SPEC>;
-#[doc = "HS Timer1 Current Value Low Register"]
-pub mod hs_tmr1_curnt_lo;
-#[doc = "HS_TMR1_CURNT_HI register accessor: an alias for `Reg<HS_TMR1_CURNT_HI_SPEC>`"]
-pub type HS_TMR1_CURNT_HI = crate::Reg<hs_tmr1_curnt_hi::HS_TMR1_CURNT_HI_SPEC>;
-#[doc = "HS Timer1 Current Value High Register"]
-pub mod hs_tmr1_curnt_hi;
+#[doc = "HS_TMR_CTRL register accessor: an alias for `Reg<HS_TMR_CTRL_SPEC>`"]
+pub type HS_TMR_CTRL = crate::Reg<hs_tmr_ctrl::HS_TMR_CTRL_SPEC>;
+#[doc = "HS Timer Control Register"]
+pub mod hs_tmr_ctrl;
+#[doc = "HS_TMR_INTV_LO register accessor: an alias for `Reg<HS_TMR_INTV_LO_SPEC>`"]
+pub type HS_TMR_INTV_LO = crate::Reg<hs_tmr_intv_lo::HS_TMR_INTV_LO_SPEC>;
+#[doc = "HS Timer Interval Value Low Register"]
+pub mod hs_tmr_intv_lo;
+#[doc = "HS_TMR_INTV_HI register accessor: an alias for `Reg<HS_TMR_INTV_HI_SPEC>`"]
+pub type HS_TMR_INTV_HI = crate::Reg<hs_tmr_intv_hi::HS_TMR_INTV_HI_SPEC>;
+#[doc = "HS Timer Interval Value High Register"]
+pub mod hs_tmr_intv_hi;
+#[doc = "HS_TMR_CURNT_LO register accessor: an alias for `Reg<HS_TMR_CURNT_LO_SPEC>`"]
+pub type HS_TMR_CURNT_LO = crate::Reg<hs_tmr_curnt_lo::HS_TMR_CURNT_LO_SPEC>;
+#[doc = "HS Timer Current Value Low Register"]
+pub mod hs_tmr_curnt_lo;
+#[doc = "HS_TMR_CURNT_HI register accessor: an alias for `Reg<HS_TMR_CURNT_HI_SPEC>`"]
+pub type HS_TMR_CURNT_HI = crate::Reg<hs_tmr_curnt_hi::HS_TMR_CURNT_HI_SPEC>;
+#[doc = "HS Timer Current Value High Register"]
+pub mod hs_tmr_curnt_hi;
--- src/hstimer/hs_tmr_irq_stas.rs
+++ src/hstimer/hs_tmr_irq_stas.rs
@@ -34,7 +34,162 @@ impl From<crate::W<HS_TMR_IRQ_STAS_SPEC>> for W {
         W(writer)
     }
 }
+#[doc = "HSTimer IRQ Pending\n\nValue on reset: 0"]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum HS_TMR_IRQ_PEND_A {
+    #[doc = "0: `0`"]
+    NO_EFFECT = 0,
+    #[doc = "1: `1`"]
+    PENDING = 1,
+}
+impl From<HS_TMR_IRQ_PEND_A> for bool {
+    #[inline(always)]
+    fn from(variant: HS_TMR_IRQ_PEND_A) -> Self {
+        variant as u8 != 0
+    }
+}
+#[doc = "Fields `HS_TMR(0-1)_IRQ_PEND` reader - HSTimer IRQ Pending"]
+pub struct HS_TMR_IRQ_PEND_R(crate::FieldReader<bool, HS_TMR_IRQ_PEND_A>);
+impl HS_TMR_IRQ_PEND_R {
+    #[inline(always)]
+    pub(crate) fn new(bits: bool) -> Self {
+        HS_TMR_IRQ_PEND_R(crate::FieldReader::new(bits))
+    }
+    #[doc = r"Get enumerated values variant"]
+    #[inline(always)]
+    pub fn variant(&self) -> HS_TMR_IRQ_PEND_A {
+        match self.bits {
+            false => HS_TMR_IRQ_PEND_A::NO_EFFECT,
+            true => HS_TMR_IRQ_PEND_A::PENDING,
+        }
+    }
+    #[doc = "Checks if the value of the field is `NO_EFFECT`"]
+    #[inline(always)]
+    pub fn is_no_effect(&self) -> bool {
+        **self == HS_TMR_IRQ_PEND_A::NO_EFFECT
+    }
+    #[doc = "Checks if the value of the field is `PENDING`"]
+    #[inline(always)]
+    pub fn is_pending(&self) -> bool {
+        **self == HS_TMR_IRQ_PEND_A::PENDING
+    }
+}
+impl core::ops::Deref for HS_TMR_IRQ_PEND_R {
+    type Target = crate::FieldReader<bool, HS_TMR_IRQ_PEND_A>;
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+#[doc = "Fields `HS_TMR(0-1)_IRQ_PEND` writer - HSTimer IRQ Pending"]
+pub struct HS_TMR_IRQ_PEND_W<'a> {
+    w: &'a mut W,
+    offset: usize,
+}
+impl<'a> HS_TMR_IRQ_PEND_W<'a> {
+    #[doc = r"Writes `variant` to the field"]
+    #[inline(always)]
+    pub fn variant(self, variant: HS_TMR_IRQ_PEND_A) -> &'a mut W {
+        self.bit(variant.into())
+    }
+    #[doc = "`0`"]
+    #[inline(always)]
+    pub fn no_effect(self) -> &'a mut W {
+        self.variant(HS_TMR_IRQ_PEND_A::NO_EFFECT)
+    }
+    #[doc = "`1`"]
+    #[inline(always)]
+    pub fn pending(self) -> &'a mut W {
+        self.variant(HS_TMR_IRQ_PEND_A::PENDING)
+    }
+    #[doc = r"Sets the field bit"]
+    #[inline(always)]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r"Clears the field bit"]
+    #[inline(always)]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r"Writes raw bits to the field"]
+    #[inline(always)]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        self.w.bits =
+            (self.w.bits & !(0x01 << self.offset)) | ((value as u32 & 0x01) << self.offset);
+        self.w
+    }
+}
+#[doc = "Fields `HS_TMR(0-1)_IRQ_PEND` const generic writer - HSTimer IRQ Pending"]
+pub struct HS_TMR_IRQ_PEND_CGW<'a, const O: usize> {
+    w: &'a mut W,
+}
+impl<'a, const O: usize> HS_TMR_IRQ_PEND_CGW<'a, O> {
+    #[doc = r"Writes `variant` to the field"]
+    #[inline(always)]
+    pub fn variant(self, variant: HS_TMR_IRQ_PEND_A) -> &'a mut W {
+        self.bit(variant.into())
+    }
+    #[doc = "`0`"]
+    #[inline(always)]
+    pub fn no_effect(self) -> &'a mut W {
+        self.variant(HS_TMR_IRQ_PEND_A::NO_EFFECT)
+    }
+    #[doc = "`1`"]
+    #[inline(always)]
+    pub fn pending(self) -> &'a mut W {
+        self.variant(HS_TMR_IRQ_PEND_A::PENDING)
+    }
+    #[doc = r"Sets the field bit"]
+    #[inline(always)]
+    pub fn set_bit(self) -> &'a mut W {
+        self.bit(true)
+    }
+    #[doc = r"Clears the field bit"]
+    #[inline(always)]
+    pub fn clear_bit(self) -> &'a mut W {
+        self.bit(false)
+    }
+    #[doc = r"Writes raw bits to the field"]
+    #[inline(always)]
+    pub fn bit(self, value: bool) -> &'a mut W {
+        self.w.bits = (self.w.bits & !(0x01 << O)) | ((value as u32 & 0x01) << O);
+        self.w
+    }
+}
+impl R {
+    #[doc = "HSTimer IRQ Pending"]
+    #[inline(always)]
+    pub unsafe fn hs_tmr_irq_pend(&self, n: usize) -> HS_TMR_IRQ_PEND_R {
+        HS_TMR_IRQ_PEND_R::new(((self.bits >> n) & 0x01) != 0)
+    }
+    #[doc = "Bit 0 - HSTimer IRQ Pending"]
+    #[inline(always)]
+    pub fn hs_tmr0_irq_pend(&self) -> HS_TMR_IRQ_PEND_R {
+        HS_TMR_IRQ_PEND_R::new((self.bits & 0x01) != 0)
+    }
+    #[doc = "Bit 1 - HSTimer IRQ Pending"]
+    #[inline(always)]
+    pub fn hs_tmr1_irq_pend(&self) -> HS_TMR_IRQ_PEND_R {
+        HS_TMR_IRQ_PEND_R::new(((self.bits >> 1) & 0x01) != 0)
+    }
+}
 impl W {
+    #[doc = "HSTimer IRQ Pending"]
+    #[inline(always)]
+    pub unsafe fn hs_tmr_irq_pend(&mut self, n: usize) -> HS_TMR_IRQ_PEND_W {
+        HS_TMR_IRQ_PEND_W { w: self, offset: n }
+    }
+    #[doc = "Bit 0 - HSTimer IRQ Pending"]
+    #[inline(always)]
+    pub fn hs_tmr0_irq_pend(&mut self) -> HS_TMR_IRQ_PEND_CGW<0> {
+        HS_TMR_IRQ_PEND_CGW { w: self }
+    }
+    #[doc = "Bit 1 - HSTimer IRQ Pending"]
+    #[inline(always)]
+    pub fn hs_tmr1_irq_pend(&mut self) -> HS_TMR_IRQ_PEND_CGW<1> {
+        HS_TMR_IRQ_PEND_CGW { w: self }
+    }
     #[doc = "Writes raw bits to the register."]
     #[inline(always)]
     pub unsafe fn bits(&mut self, bits: u32) -> &mut Self {
