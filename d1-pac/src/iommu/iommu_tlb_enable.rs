3792c6c868354ad41fab2b4912bd663e066ed430
Campbell He <kp.campbell.he@duskmoon314.com>
2022-07-24 12:24:48 +0800

    style(d1-pac): remove redundant space in description

--- d1-pac/src/iommu/iommu_tlb_enable.rs
+++ d1-pac/src/iommu/iommu_tlb_enable.rs
@@ -137,7 +137,7 @@ impl<'a, const O: u8> MACRO_TLB_ENABLE_W<'a, O> {
     }
 }
 #[doc = "Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable\n\nValue on reset: 1"]
+enable bit\n\nValue on reset: 1"]
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum MICRO_TLB_ENABLE_A {
     #[doc = "0: Disable"]
@@ -152,7 +152,7 @@ impl From<MICRO_TLB_ENABLE_A> for bool {
     }
 }
 #[doc = "Fields `micro_tlb(0-6)_enable` reader - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
 pub type MICRO_TLB_ENABLE_R = crate::BitReader<MICRO_TLB_ENABLE_A>;
 impl MICRO_TLB_ENABLE_R {
     #[doc = "Get enumerated values variant"]
@@ -175,7 +175,7 @@ impl MICRO_TLB_ENABLE_R {
     }
 }
 #[doc = "Fields `micro_tlb(0-6)_enable` writer - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
 pub type MICRO_TLB_ENABLE_W<'a, const O: u8> =
     crate::BitWriter<'a, u32, IOMMU_TLB_ENABLE_SPEC, MICRO_TLB_ENABLE_A, O>;
 impl<'a, const O: u8> MICRO_TLB_ENABLE_W<'a, O> {
@@ -202,49 +202,49 @@ impl R {
         MACRO_TLB_ENABLE_R::new(((self.bits >> 16) & 1) != 0)
     }
     #[doc = "Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub unsafe fn micro_tlb_enable(&self, n: u8) -> MICRO_TLB_ENABLE_R {
         MICRO_TLB_ENABLE_R::new(((self.bits >> n) & 1) != 0)
     }
     #[doc = "Bit 0 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb0_enable(&self) -> MICRO_TLB_ENABLE_R {
         MICRO_TLB_ENABLE_R::new((self.bits & 1) != 0)
     }
     #[doc = "Bit 1 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb1_enable(&self) -> MICRO_TLB_ENABLE_R {
         MICRO_TLB_ENABLE_R::new(((self.bits >> 1) & 1) != 0)
     }
     #[doc = "Bit 2 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb2_enable(&self) -> MICRO_TLB_ENABLE_R {
         MICRO_TLB_ENABLE_R::new(((self.bits >> 2) & 1) != 0)
     }
     #[doc = "Bit 3 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb3_enable(&self) -> MICRO_TLB_ENABLE_R {
         MICRO_TLB_ENABLE_R::new(((self.bits >> 3) & 1) != 0)
     }
     #[doc = "Bit 4 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb4_enable(&self) -> MICRO_TLB_ENABLE_R {
         MICRO_TLB_ENABLE_R::new(((self.bits >> 4) & 1) != 0)
     }
     #[doc = "Bit 5 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb5_enable(&self) -> MICRO_TLB_ENABLE_R {
         MICRO_TLB_ENABLE_R::new(((self.bits >> 5) & 1) != 0)
     }
     #[doc = "Bit 6 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb6_enable(&self) -> MICRO_TLB_ENABLE_R {
         MICRO_TLB_ENABLE_R::new(((self.bits >> 6) & 1) != 0)
@@ -262,49 +262,49 @@ impl W {
         MACRO_TLB_ENABLE_W::new(self)
     }
     #[doc = "Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub unsafe fn micro_tlb_enable<const O: u8>(&mut self) -> MICRO_TLB_ENABLE_W<O> {
         MICRO_TLB_ENABLE_W::new(self)
     }
     #[doc = "Bit 0 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb0_enable(&mut self) -> MICRO_TLB_ENABLE_W<0> {
         MICRO_TLB_ENABLE_W::new(self)
     }
     #[doc = "Bit 1 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb1_enable(&mut self) -> MICRO_TLB_ENABLE_W<1> {
         MICRO_TLB_ENABLE_W::new(self)
     }
     #[doc = "Bit 2 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb2_enable(&mut self) -> MICRO_TLB_ENABLE_W<2> {
         MICRO_TLB_ENABLE_W::new(self)
     }
     #[doc = "Bit 3 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb3_enable(&mut self) -> MICRO_TLB_ENABLE_W<3> {
         MICRO_TLB_ENABLE_W::new(self)
     }
     #[doc = "Bit 4 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb4_enable(&mut self) -> MICRO_TLB_ENABLE_W<4> {
         MICRO_TLB_ENABLE_W::new(self)
     }
     #[doc = "Bit 5 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb5_enable(&mut self) -> MICRO_TLB_ENABLE_W<5> {
         MICRO_TLB_ENABLE_W::new(self)
     }
     #[doc = "Bit 6 - Micro TLB\\[i\\]
-enable bit 0: Disable 1: Enable"]
+enable bit"]
     #[inline(always)]
     pub fn micro_tlb6_enable(&mut self) -> MICRO_TLB_ENABLE_W<6> {
         MICRO_TLB_ENABLE_W::new(self)
